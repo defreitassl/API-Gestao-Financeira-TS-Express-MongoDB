60c449a384daf65bbd86d2e73b988e2d52fdeda4
Douglas Freitas <douglasamf14@gmail.com>
2025-04-24 14:11:38 -0300

    Feature: now, when updated a transaction amount, the user balance is recalculated imediatelly

--- src/services/TransactionService.ts
+++ src/services/TransactionService.ts
@@ -121,6 +121,25 @@ class TransactionService{
             const transactionId: Types.ObjectId | false = toObjectId(transactionIdParam)
             if (!transactionId) throw new BadRequestError("Invalid transaction Id param")
 
+            const oldTransactionAmount: ITransaction | null = await TransactionRepository.getOneTransaction(transactionId, userId)
+            if (!oldTransactionAmount) throw new NotFoundError("Transaction not found")
+
+            if (data.amount && data.amount !== oldTransactionAmount.amount) {
+                const user: IUser | null = await UserRepository.getOne(userId)
+                if (!user) throw new NotFoundError("Could not found user")
+
+                const newUserBalanceAmount: number = oldTransactionAmount.inflow
+                ? user.balance + (data.amount - oldTransactionAmount.amount)
+                : user.balance - (data.amount - oldTransactionAmount.amount)
+
+                const updatedUserInfo: UpdateResult = await UserRepository.updateUserBalance(newUserBalanceAmount, userId)
+
+                if ( // Verifies if mongoose could find and modify the user balance
+                    updatedUserInfo.matchedCount === 0
+                    || updatedUserInfo.modifiedCount === 0
+                ) throw new NotFoundError("Couldn't update user Balance")
+            }
+
             const updatedTransactionInfo: UpdateResult = await TransactionRepository.updateOneTransaction(transactionId, userId, data)
 
             if ( // Verifies if mongoose could find and modify the transaction
